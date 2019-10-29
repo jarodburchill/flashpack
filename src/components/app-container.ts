b18597e05e3dd86c33a702df9b4c8c5aba4eca57
Jarod Burchill <jarodburchill@gmail.com>
2019-10-29 15:29:46 -0400

    Tested conditional rendering in lit-element

--- src/components/app-container.ts
+++ src/components/app-container.ts
@@ -1,16 +1,31 @@
-import { customElement, html, LitElement } from "lit-element";
+import { customElement, html, LitElement, property } from "lit-element";
 import { globalStyles } from "./styles/global-styles";
 import "./test-container";
 
 @customElement("app-container")
 export class AppContainer extends LitElement {
+  @property() bool: boolean = false;
+  @property() name: string = "Test";
+  contentDesc = html`
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic aspernatur
+      quidem animi id cupiditate repellat sit. Asperiores delectus nam
+      praesentium consequatur, veritatis fugiat omnis. Aut voluptatum qui amet
+      delectus libero!
+    </p>
+  `;
+  handleClick() {
+    this.bool = !this.bool;
+  }
   static get styles() {
     return globalStyles;
   }
   render() {
     return html`
       <div>
-        <h1>Test</h1>
+        <h1>${this.name}</h1>
+        <button @click="${this.handleClick}">Test</button>
+        ${this.bool ? this.contentDesc : null}
         <test-container></test-container>
       </div>
     `;
