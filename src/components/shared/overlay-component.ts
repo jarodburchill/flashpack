f3ea23d1382cd618165562ee4b41efa05fb80c96
Jarod Burchill <jarodburchill@gmail.com>
2019-11-06 09:22:42 -0500

    Added contrast prop to component

--- src/components/shared/overlay-component.ts
+++ src/components/shared/overlay-component.ts
@@ -4,9 +4,10 @@ import { globalStyles } from "../../styles/global-styles";
 
 @customElement("overlay-component")
 export class OverlayComponent extends LitElement {
+  @property({ type: Boolean }) contrast = false;
   @property({ type: Boolean }) frosted = false;
-  @property({ type: Boolean }) rounded = false;
   @property({ type: Boolean }) marginless = false;
+  @property({ type: Boolean }) rounded = false;
   static get styles() {
     return [
       globalStyles,
@@ -19,6 +20,10 @@ export class OverlayComponent extends LitElement {
           margin: 5vh 2.5vw;
           padding: 5vh;
           width: 95%;
+        }
+        .contrast {
+          background: var(--theme-fore);
+          color: var(--theme-back);
         }
         .frosted {
           position: relative;
@@ -39,12 +44,31 @@ export class OverlayComponent extends LitElement {
           margin: -20px;
           z-index: -1;
         }
-        .rounded {
-          border-radius: 10px;
+        .frostedContrast {
+          position: relative;
+          background: inherit;
+          overflow: hidden;
+          z-index: 1;
+        }
+        .frostedContrast::before {
+          content: "";
+          position: absolute;
+          background: inherit;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          box-shadow: inset 0 0 0 2000px var(--theme-frost-con);
+          filter: blur(10px);
+          margin: -20px;
+          z-index: -1;
         }
         .marginless {
           margin: 0;
           width: 100%;
+        }
+        .rounded {
+          border-radius: 10px;
         }
       `,
     ];
@@ -53,7 +77,9 @@ export class OverlayComponent extends LitElement {
     return html`
       <div
         class=${classMap({
-          frosted: this.frosted,
+          contrast: this.contrast,
+          frosted: this.contrast && this.frosted ? false : this.frosted,
+          frostedContrast: this.contrast && this.frosted ? true : false,
           marginless: this.marginless,
           overlay: true,
           rounded: this.rounded,
