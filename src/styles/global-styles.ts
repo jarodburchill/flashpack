c309dfa41efda7a38d62c76d8aae2d2169207788
Jarod Burchill <jarodburchill@gmail.com>
2019-11-06 09:21:57 -0500

    Moved colors to :host selector

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -6,15 +6,20 @@ const darkMode: boolean = store.get("darkMode");
 
 export const globalStyles = css`
   * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  :host {
     --theme-fore: ${darkMode ? css`hsl(0, 0%, 100%)` : css`hsl(0, 0%, 0%)`};
     --theme-back: ${darkMode ? css`hsl(0, 0%, 0%)` : css`hsl(0, 0%, 100%)`};
     --theme-alt: ${darkMode ? css`hsl(0, 0%, 25%)` : css`hsl(0, 0%, 75%)`};
     --theme-frost: ${darkMode
       ? css`hsla(0, 0%, 0%, 0.75)`
       : css`hsla(0, 0%, 100%, 0.75)`};
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    --theme-frost-con: ${darkMode
+      ? css`hsla(0, 0%, 100%, 0.75)`
+      : css`hsla(0, 0%, 0%, 0.75)`};
     color: var(--theme-fore);
   }
 `;
