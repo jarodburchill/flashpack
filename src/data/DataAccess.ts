45ad07fc641b0f928d42ecece68291953d10099e
Jarod Burchill <jarodburchill@gmail.com>
2019-11-12 09:41:53 -0500

    Added DAL CRUD functions for schema's groups prop

--- src/data/DataAccess.ts
+++ src/data/DataAccess.ts
@@ -1,4 +1,5 @@
 import Store = require("electron-store");
+import { IGroup } from "src/models/Group";
 import { ISchema } from "../models/Schema";
 
 export class DataAccess {
@@ -25,7 +26,39 @@ export class DataAccess {
   setDarkMode(darkMode: boolean) {
     this.store.set("darkMode", darkMode);
   }
-  getCards() {
-    return this.store.get("cards");
+  getGroups() {
+    return this.store.get("groups");
+  }
+  // should be private?
+  setGroups(groups: Array<IGroup>) {
+    this.store.set("groups", groups);
+  }
+  addGroup(group: IGroup) {
+    const groups = this.getGroups();
+    groups.push(group);
+    this.setGroups(groups);
+  }
+  getGroup(id: number) {
+    const groups = this.getGroups();
+    const requestedGroup = groups.find((group: IGroup) => {
+      return group.id === id;
+    });
+    return requestedGroup;
+  }
+  updateGroup(updatedGroup: IGroup) {
+    const groups = this.getGroups();
+    const updateIndex = groups.findIndex((group: IGroup) => {
+      return group.id === updatedGroup.id;
+    });
+    groups[updateIndex] = updatedGroup;
+    this.setGroups(groups);
+  }
+  removeGroup(id: number) {
+    const groups = this.getGroups();
+    const removeIndex = groups.findIndex((group: IGroup) => {
+      return group.id === id;
+    });
+    groups.splice(removeIndex, 1);
+    this.setGroups(groups);
   }
 }
