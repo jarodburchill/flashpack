2d424d9c1cbeef1513efde17dfad0e98d666bee4
Jarod Burchill <jarodburchill@gmail.com>
2019-11-15 08:42:51 -0500

    Removed test

--- tests/DarkModeDAL.test.ts
+++
@@ -1,7 +0,0 @@
-import { DarkModeDAL } from "../src/data/classes/DarkModeDAL";
-
-it("stores darkMode", () => {
-  const darkModeDAL: DarkModeDAL = new DarkModeDAL();
-  darkModeDAL.setDarkMode(true);
-  expect(darkModeDAL.getDarkMode()).toBe(true);
-});
