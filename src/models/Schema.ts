b04c585a2eacf537f888f6ec8aba9fe6a39b5481
Jarod Burchill <jarodburchill@gmail.com>
2019-11-18 15:55:50 -0500

    Added schema string indexing

--- src/models/Schema.ts
+++ src/models/Schema.ts
@@ -3,7 +3,9 @@ import { IGroup } from "./Group";
 import { IPack } from "./Pack";
 import { IQuizcard } from "./Quizcard";
 
+type Types = (IFlashcard | IQuizcard)[] | boolean | IGroup[] | number | IPack[];
 export interface ISchema {
+  [index: string]: Types;
   cards: (IFlashcard | IQuizcard)[];
   darkMode: boolean;
   groups: IGroup[];
