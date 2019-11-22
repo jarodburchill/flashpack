1d8b83531387cee47abe01dc9ad8503113094cea
Jarod Burchill <jarodburchill@gmail.com>
2019-11-22 15:56:18 -0500

    Removed indexing

--- src/models/Schema.ts
+++ src/models/Schema.ts
@@ -3,9 +3,7 @@ import { IGroup } from "./Group";
 import { IPack } from "./Pack";
 import { IQuizcard } from "./Quizcard";
 
-type Types = (IFlashcard | IQuizcard)[] | boolean | IGroup[] | number | IPack[];
 export interface ISchema {
-  [index: string]: Types;
   cards: (IFlashcard | IQuizcard)[];
   darkMode: boolean;
   groups: IGroup[];
