7cb18071840a14e11868e10ec613c934a90373e4
Jarod Burchill <jarodburchill@gmail.com>
2019-11-27 10:54:00 -0500

    Added type aliases

--- src/data/classes/CardsDAL.ts
+++ src/data/classes/CardsDAL.ts
@@ -1,22 +1,23 @@
-import { IFlashcard } from "src/models/Flashcard";
+import { IFlashcard, INewFlashcard } from "src/models/Flashcard";
 import { IPack } from "src/models/Pack";
-import { IQuizcard } from "src/models/Quizcard";
+import { INewQuizcard, IQuizcard } from "src/models/Quizcard";
 import { BaseDAL } from "./BaseDAL";
 
+type Card = IFlashcard | IQuizcard;
+type NewCard = INewFlashcard | INewQuizcard;
+
 export class CardsDAL extends BaseDAL {
-  private getCards(): (IFlashcard | IQuizcard)[] {
+  private getCards(): Card[] {
     return this.electronStore.get("cards");
   }
-  private setCards(cards: (IFlashcard | IQuizcard)[]): void {
+  private setCards(cards: Card[]): void {
     this.electronStore.set("cards", cards);
   }
-  public getPackCards<T extends IFlashcard | IQuizcard>(pack: IPack): T[] {
-    const cards: (IFlashcard | IQuizcard)[] = this.getCards();
-    const packCards: (IFlashcard | IQuizcard)[] = cards.filter(
-      (card: IFlashcard | IQuizcard) => {
-        return card.packId === pack.id;
-      }
-    );
+  public getPackCards<T extends Card>(pack: IPack): T[] {
+    const cards: Card[] = this.getCards();
+    const packCards: Card[] = cards.filter((card: Card) => {
+      return card.packId === pack.id;
+    });
     return packCards as T[];
   }
 }
