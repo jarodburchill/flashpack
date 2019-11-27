f8d006cb1d610f2989c748d80da6f103b1bae981
Jarod Burchill <jarodburchill@gmail.com>
2019-11-27 09:04:49 -0500

    Added getPackCards function

--- src/data/classes/CardsDAL.ts
+++ src/data/classes/CardsDAL.ts
@@ -1,4 +1,5 @@
 import { IFlashcard } from "src/models/Flashcard";
+import { IPack } from "src/models/Pack";
 import { IQuizcard } from "src/models/Quizcard";
 import { BaseDAL } from "./BaseDAL";
 
@@ -8,5 +9,14 @@ export class CardsDAL extends BaseDAL {
   }
   private setCards(cards: (IFlashcard | IQuizcard)[]): void {
     this.electronStore.set("cards", cards);
+  }
+  public getPackCards<T extends IFlashcard | IQuizcard>(pack: IPack): T[] {
+    const cards: (IFlashcard | IQuizcard)[] = this.getCards();
+    const packCards: (IFlashcard | IQuizcard)[] = cards.filter(
+      (card: IFlashcard | IQuizcard) => {
+        return card.packId === pack.id;
+      }
+    );
+    return packCards as T[];
   }
 }
