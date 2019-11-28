84df8b63306edf870b158f4cfa5d0244f70a9277
Jarod Burchill <jarodburchill@gmail.com>
2019-11-28 12:20:41 -0500

    Mock writing to JSON file

--- tests/__mocks__/electron-store.ts
+++ tests/__mocks__/electron-store.ts
@@ -4,10 +4,12 @@ class ElectronStore<T = any> {
     this.store = options.defaults;
   }
   public get(key: string): any {
+    this.store = JSON.parse(JSON.stringify(this.store));
     return this.store[key];
   }
   public set(key: string, value: any): void {
     this.store[key] = value;
+    this.store = JSON.parse(JSON.stringify(this.store));
   }
 }
 
