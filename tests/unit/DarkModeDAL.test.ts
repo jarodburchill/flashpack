4f7022a9c0a4b802ed55744b97b2688b07406ba7
Jarod Burchill <jarodburchill@gmail.com>
2019-12-02 09:30:16 -0500

    Refactored tests

--- tests/unit/DarkModeDAL.test.ts
+++ tests/unit/DarkModeDAL.test.ts
@@ -1,24 +1,13 @@
 import ElectronStore = require("electron-store");
 import { DarkModeDAL } from "../../src/data/classes/DarkModeDAL";
 import { ISchema } from "../../src/models/Schema";
+import { getEmptyStore } from "../testData";
 
 jest.mock("electron-store");
 
-const getStore: () => ElectronStore<ISchema> = () => {
-  return new ElectronStore<ISchema>({
-    defaults: {
-      cards: [],
-      darkMode: false,
-      groups: [],
-      nextId: 1,
-      packs: [],
-    },
-  });
-};
-
 describe("getDarkMode", () => {
-  it("gets a storred darkMode value", () => {
-    const electronStore: ElectronStore<ISchema> = getStore();
+  it("gets a stored darkMode value", () => {
+    const electronStore: ElectronStore<ISchema> = getEmptyStore();
     const darkModeDAL: DarkModeDAL = new DarkModeDAL(electronStore);
     expect(darkModeDAL.getDarkMode()).toBe(false);
   });
@@ -26,7 +15,7 @@ describe("getDarkMode", () => {
 
 describe("setDarkMode", () => {
   it("sets a darkMode value in storage", () => {
-    const electronStore: ElectronStore<ISchema> = getStore();
+    const electronStore: ElectronStore<ISchema> = getEmptyStore();
     const darkModeDAL: DarkModeDAL = new DarkModeDAL(electronStore);
     darkModeDAL.setDarkMode(true);
     expect(electronStore.store.darkMode).toBe(true);
