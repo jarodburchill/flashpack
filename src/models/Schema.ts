d47d2f6445bd6b09dd1c5548c1d0507e16c89c41
Jarod Burchill <jarodburchill@gmail.com>
2019-12-02 10:56:15 -0500

    Added Card type alias to Schema

--- src/models/Schema.ts
+++ src/models/Schema.ts
@@ -3,10 +3,12 @@ import { IGroup } from "./Group";
 import { IPack } from "./Pack";
 import { IQuizcard } from "./Quizcard";
 
+export type Card = IFlashcard | IQuizcard;
+
 export interface ISchema {
   darkMode: boolean;
   nextId: number;
   groups: IGroup[];
   packs: IPack[];
-  cards: (IFlashcard | IQuizcard)[];
+  cards: Card[];
 }
