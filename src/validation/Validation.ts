6af1844336c9f282f5c609d9b0a5dfa47ac0ca93
Jarod Burchill <jarodburchill@gmail.com>
2019-12-04 13:14:14 -0500

    Completed Pack validation function

--- src/validation/Validation.ts
+++ src/validation/Validation.ts
@@ -2,6 +2,11 @@ import _ = require("lodash");
 import { IGroup } from "src/models/Group";
 import { IPack } from "src/models/Pack";
 
+enum PackType {
+  flash = "flash",
+  quiz = "quiz",
+}
+
 export abstract class Validation {
   private static minNameLength: number = 5;
   public static isValidGroup(group: IGroup, errorsRef: string[]): boolean {
@@ -36,11 +41,28 @@ export abstract class Validation {
     if (!_.isInteger(pack.groupId)) {
       errorsRef.push("GroupID must be an integer.");
     }
+    if (!(pack.type in PackType)) {
+      errorsRef.push("Type must be either 'flash' or 'quiz'.");
+    }
+    if (
+      pack.type === PackType.flash &&
+      (pack.timed === true || pack.liveResults === true)
+    ) {
+      errorsRef.push(
+        "If type is 'flash' then timed and liveResults must be false."
+      );
+    }
     if (_.isEmpty(pack.name)) {
-      errorsRef.push("Name cannot be empty");
+      errorsRef.push("Name cannot be empty.");
     }
     if (pack.name.length < this.minNameLength) {
-      errorsRef.push("Name must be at least 5 characters");
+      errorsRef.push("Name must be at least 5 characters.");
+    }
+    if (!_.isBoolean(pack.timed)) {
+      errorsRef.push("Timed must be a boolean.");
+    }
+    if (!_.isBoolean(pack.liveResults)) {
+      errorsRef.push("LiveResults must be a boolean.");
     }
     return _.isEmpty(errorsRef);
   }
