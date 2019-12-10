bf7beb4b481885e5a4d645e53b74cdfbd0c30722
Jarod Burchill <jarodburchill@gmail.com>
2019-12-10 15:17:12 -0500

    Added Flashcard validation and refactored

--- src/validation/Validation.ts
+++ src/validation/Validation.ts
@@ -1,14 +1,31 @@
 import _ = require("lodash");
+import { IFlashcard } from "src/models/Flashcard";
+import { IQuizcard } from "src/models/Quizcard";
 import { IGroup } from "../models/Group";
 import { IPack } from "../models/Pack";
+import { IQuizAnswer } from "../models/QuizAnswer";
 
-enum PackType {
+enum Type {
   flash = "flash",
   quiz = "quiz",
 }
 
+enum QuizType {
+  multipleChoice = "mc",
+  trueFalse = "tf",
+  checkbox = "chk",
+  blank = "blank",
+}
+
 export abstract class Validation {
   private static minNameLength: number = 2;
+  private static correctAnswers: number = 1;
+  private static trueFalseAnswers: number = 2;
+  private static minAnswers: number = 3;
+  private static maxAnswers: number = 24;
+  private static minBlanks: number = 1;
+  private static maxBlanks: number = 24;
+  private static blanksRegExp: RegExp = /\$_\$/g;
   public static isValidGroup(group: IGroup, errorsRef: string[]): boolean {
     if (!_.isEmpty(errorsRef)) {
       throw new Error(
@@ -41,13 +58,10 @@ export abstract class Validation {
     if (!_.isInteger(pack.groupId)) {
       errorsRef.push("GroupID must be an integer");
     }
-    if (!(pack.type in PackType)) {
+    if (!(pack.type in Type)) {
       errorsRef.push("Type must be either 'flash' or 'quiz'");
     }
-    if (
-      pack.type === PackType.flash &&
-      (pack.timed === true || pack.liveResults === true)
-    ) {
+    if (pack.type === Type.flash && (pack.timed || pack.liveResults)) {
       errorsRef.push(
         "If type is 'flash' then timed and liveResults must be false"
       );
@@ -63,6 +77,41 @@ export abstract class Validation {
     }
     if (!_.isBoolean(pack.liveResults)) {
       errorsRef.push("LiveResults must be a boolean");
+    }
+    return _.isEmpty(errorsRef);
+  }
+  public static isValidFlashcard(
+    flashcard: IFlashcard,
+    errorsRef: string[]
+  ): boolean {
+    if (!_.isEmpty(errorsRef)) {
+      throw new Error(
+        "errorsRef parameter must be passed in as an empty string array"
+      );
+    }
+    if (!_.isInteger(flashcard.id)) {
+      errorsRef.push("ID must be an integer");
+    }
+    if (!_.isInteger(flashcard.packId)) {
+      errorsRef.push("PackID must be an integer");
+    }
+    if (flashcard.type !== Type.flash) {
+      errorsRef.push("Type must be 'flash'");
+    }
+    if (!_.isString(flashcard.term)) {
+      errorsRef.push("Term must be a string");
+    }
+    if (_.isEmpty(flashcard.term)) {
+      errorsRef.push("Term cannot be empty");
+    }
+    if (!_.isString(flashcard.definition)) {
+      errorsRef.push("Definition must be a string");
+    }
+    if (_.isEmpty(flashcard.definition)) {
+      errorsRef.push("Definition cannot be empty");
+    }
+    if (!_.isBoolean(flashcard.starred)) {
+      errorsRef.push("Starred must be a boolean");
     }
     return _.isEmpty(errorsRef);
   }
