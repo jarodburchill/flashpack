0f5c1c370b77d7c3629a91630c2ebe41471c4142
Jarod Burchill <jarodburchill@gmail.com>
2019-12-16 15:46:42 -0500

    Added addCard unit tests

--- tests/unit/CardsDAL.test.ts
+++ tests/unit/CardsDAL.test.ts
@@ -419,3 +419,372 @@ describe("getQuizcard", () => {
     );
   });
 });
+
+describe("addCard", () => {
+  it("adds a new flashcard to an existing pack", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = electronStore.store.packs[0];
+    cardsDAL.addCard(pack, {
+      type: "flash",
+      term: "Term",
+      definition: "Def",
+      starred: false,
+    });
+    expect(electronStore.store.cards).toEqual([
+      {
+        id: 8,
+        packId: 4,
+        type: "flash",
+        term: "2 + 2",
+        definition: "4",
+        starred: false,
+      },
+      {
+        id: 9,
+        packId: 4,
+        type: "flash",
+        term: "2 - 2",
+        definition: "0",
+        starred: true,
+      },
+      {
+        id: 10,
+        packId: 6,
+        type: "quiz",
+        quizType: "mc",
+        question: "what is the symbol for water?",
+        answers: [
+          { text: "H3O", correct: false },
+          { text: "H2O", correct: true },
+          { text: "B2O", correct: false },
+          { text: "W2O", correct: false },
+        ],
+        starred: false,
+      },
+      {
+        id: 11,
+        packId: 6,
+        type: "quiz",
+        quizType: "tf",
+        question: "water boils at 100 degrees celsius.",
+        answers: [
+          { text: "false", correct: false },
+          { text: "true", correct: true },
+        ],
+        starred: true,
+      },
+      {
+        id: 12,
+        packId: 6,
+        type: "quiz",
+        quizType: "chk",
+        question: "which of the following are states of matter.",
+        answers: [
+          { text: "Liquid", correct: true },
+          { text: "Gas", correct: true },
+          { text: "Gum", correct: false },
+          { text: "Sticky", correct: false },
+          { text: "Solid", correct: true },
+        ],
+        starred: false,
+      },
+      {
+        id: 13,
+        packId: 6,
+        type: "quiz",
+        quizType: "blank",
+        question: "$_$ is the study of the natural $_$.",
+        answers: [
+          { text: "Science", correct: true },
+          { text: "word", correct: true },
+        ],
+        starred: true,
+      },
+      {
+        id: 14,
+        packId: 4,
+        type: "flash",
+        term: "Term",
+        definition: "Def",
+        starred: false,
+      },
+    ]);
+  });
+  it("adds a new quizcard to an existing pack", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = electronStore.store.packs[3];
+    cardsDAL.addCard(pack, {
+      type: "quiz",
+      quizType: "tf",
+      question: "Q",
+      answers: [
+        { text: "false", correct: false },
+        { text: "true", correct: true },
+      ],
+      starred: false,
+    });
+    expect(electronStore.store.cards).toEqual([
+      {
+        id: 8,
+        packId: 4,
+        type: "flash",
+        term: "2 + 2",
+        definition: "4",
+        starred: false,
+      },
+      {
+        id: 9,
+        packId: 4,
+        type: "flash",
+        term: "2 - 2",
+        definition: "0",
+        starred: true,
+      },
+      {
+        id: 10,
+        packId: 6,
+        type: "quiz",
+        quizType: "mc",
+        question: "what is the symbol for water?",
+        answers: [
+          { text: "H3O", correct: false },
+          { text: "H2O", correct: true },
+          { text: "B2O", correct: false },
+          { text: "W2O", correct: false },
+        ],
+        starred: false,
+      },
+      {
+        id: 11,
+        packId: 6,
+        type: "quiz",
+        quizType: "tf",
+        question: "water boils at 100 degrees celsius.",
+        answers: [
+          { text: "false", correct: false },
+          { text: "true", correct: true },
+        ],
+        starred: true,
+      },
+      {
+        id: 12,
+        packId: 6,
+        type: "quiz",
+        quizType: "chk",
+        question: "which of the following are states of matter.",
+        answers: [
+          { text: "Liquid", correct: true },
+          { text: "Gas", correct: true },
+          { text: "Gum", correct: false },
+          { text: "Sticky", correct: false },
+          { text: "Solid", correct: true },
+        ],
+        starred: false,
+      },
+      {
+        id: 13,
+        packId: 6,
+        type: "quiz",
+        quizType: "blank",
+        question: "$_$ is the study of the natural $_$.",
+        answers: [
+          { text: "Science", correct: true },
+          { text: "word", correct: true },
+        ],
+        starred: true,
+      },
+      {
+        id: 14,
+        packId: 7,
+        type: "quiz",
+        quizType: "tf",
+        question: "Q",
+        answers: [
+          { text: "false", correct: false },
+          { text: "true", correct: true },
+        ],
+        starred: false,
+      },
+    ]);
+  });
+  it("auto increments next id upon adding a card", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = electronStore.store.packs[0];
+    cardsDAL.addCard(pack, {
+      type: "flash",
+      term: "Flash 1",
+      definition: "Def",
+      starred: false,
+    });
+    cardsDAL.addCard(pack, {
+      type: "flash",
+      term: "Flash 2",
+      definition: "Def",
+      starred: false,
+    });
+    expect(electronStore.store.cards).toEqual([
+      {
+        id: 8,
+        packId: 4,
+        type: "flash",
+        term: "2 + 2",
+        definition: "4",
+        starred: false,
+      },
+      {
+        id: 9,
+        packId: 4,
+        type: "flash",
+        term: "2 - 2",
+        definition: "0",
+        starred: true,
+      },
+      {
+        id: 10,
+        packId: 6,
+        type: "quiz",
+        quizType: "mc",
+        question: "what is the symbol for water?",
+        answers: [
+          { text: "H3O", correct: false },
+          { text: "H2O", correct: true },
+          { text: "B2O", correct: false },
+          { text: "W2O", correct: false },
+        ],
+        starred: false,
+      },
+      {
+        id: 11,
+        packId: 6,
+        type: "quiz",
+        quizType: "tf",
+        question: "water boils at 100 degrees celsius.",
+        answers: [
+          { text: "false", correct: false },
+          { text: "true", correct: true },
+        ],
+        starred: true,
+      },
+      {
+        id: 12,
+        packId: 6,
+        type: "quiz",
+        quizType: "chk",
+        question: "which of the following are states of matter.",
+        answers: [
+          { text: "Liquid", correct: true },
+          { text: "Gas", correct: true },
+          { text: "Gum", correct: false },
+          { text: "Sticky", correct: false },
+          { text: "Solid", correct: true },
+        ],
+        starred: false,
+      },
+      {
+        id: 13,
+        packId: 6,
+        type: "quiz",
+        quizType: "blank",
+        question: "$_$ is the study of the natural $_$.",
+        answers: [
+          { text: "Science", correct: true },
+          { text: "word", correct: true },
+        ],
+        starred: true,
+      },
+      {
+        id: 14,
+        packId: 4,
+        type: "flash",
+        term: "Flash 1",
+        definition: "Def",
+        starred: false,
+      },
+      {
+        id: 15,
+        packId: 4,
+        type: "flash",
+        term: "Flash 2",
+        definition: "Def",
+        starred: false,
+      },
+    ]);
+  });
+  it("throws an error when flashcard is invalid", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = electronStore.store.packs[0];
+    expect(() => {
+      cardsDAL.addCard(pack, {
+        type: "flash",
+        term: "",
+        definition: "",
+        starred: false,
+      });
+    }).toThrow(
+      new Error(
+        "Invalid Card:" +
+          "\nTerm cannot be empty," +
+          "\nDefinition cannot be empty."
+      )
+    );
+  });
+  it("throws an error when quizcard is invalid", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = electronStore.store.packs[3];
+    expect(() => {
+      cardsDAL.addCard(pack, {
+        type: "quiz",
+        quizType: "mc",
+        question: "",
+        answers: [],
+        starred: false,
+      });
+    }).toThrow(
+      new Error(
+        "Invalid Card:" +
+          "\nQuestion cannot be empty," +
+          "\nMultiple Choice Quizcards must contain between 3-24 answer objects," +
+          "\nMultiple Choice Quizcards must have exactly 1 correct answer."
+      )
+    );
+  });
+  it("throws an error when card type does not equal pack type", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = electronStore.store.packs[0];
+    expect(() => {
+      cardsDAL.addCard(pack, {
+        type: "quiz",
+        quizType: "tf",
+        question: "Q",
+        answers: [
+          { text: "false", correct: false },
+          { text: "true", correct: true },
+        ],
+        starred: false,
+      });
+    }).toThrow(new Error("Card type and Pack type must match."));
+  });
+  it("throws an error when attempting to add a card to a non existing pack", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = electronStore.store.packs[0];
+    expect(() => {
+      cardsDAL.addCard(pack, {
+        type: "quiz",
+        quizType: "tf",
+        question: "Q",
+        answers: [
+          { text: "false", correct: false },
+          { text: "true", correct: true },
+        ],
+        starred: false,
+      });
+    }).toThrow(new Error("Card type and Pack type must match."));
+  });
+});
