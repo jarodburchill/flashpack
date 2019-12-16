266f871ccd602829cf074fe518f6556cc442866a
Jarod Burchill <jarodburchill@gmail.com>
2019-12-16 11:06:19 -0500

    Added getPackQuizcards unit tests

--- tests/unit/CardsDAL.test.ts
+++ tests/unit/CardsDAL.test.ts
@@ -113,13 +113,13 @@ describe("getPackFlashcards", () => {
       },
     ]);
   });
-  it("gets an empty array for an existing pack that has no cards", () => {
+  it("gets an empty array for an existing pack that has no flashcards", () => {
     const electronStore: ElectronStore<ISchema> = getPopulatedStore();
-    const packsDAL: CardsDAL = new CardsDAL(electronStore);
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
     const pack: IPack = electronStore.store.packs[1];
-    expect(packsDAL.getPackFlashcards(pack)).toEqual([]);
+    expect(cardsDAL.getPackFlashcards(pack)).toEqual([]);
   });
-  it("throws an error when attempting to get cards for a non-existing pack", () => {
+  it("throws an error when attempting to get flashcards for a non-existing pack", () => {
     const electronStore: ElectronStore<ISchema> = getEmptyStore();
     const cardsDAL: CardsDAL = new CardsDAL(electronStore);
     const pack: IPack = {
@@ -134,7 +134,7 @@ describe("getPackFlashcards", () => {
       cardsDAL.getPackFlashcards(pack);
     }).toThrow(new Error("Could not find matching Pack to get Cards from."));
   });
-  it("throws an error when attempting to get cards for an existing pack with modified values", () => {
+  it("throws an error when attempting to get flashcards for an existing pack with modified values", () => {
     const electronStore: ElectronStore<ISchema> = getPopulatedStore();
     const cardsDAL: CardsDAL = new CardsDAL(electronStore);
     const pack: IPack = {
@@ -149,7 +149,7 @@ describe("getPackFlashcards", () => {
       cardsDAL.getPackFlashcards(pack);
     }).toThrow(new Error("Could not find matching Pack to get Cards from."));
   });
-  it("throws an error when attempting to get cards for a pack with a type of 'quiz'", () => {
+  it("throws an error when attempting to get flashcards for a pack with a type of 'quiz'", () => {
     const electronStore: ElectronStore<ISchema> = getPopulatedStore();
     const cardsDAL: CardsDAL = new CardsDAL(electronStore);
     const pack: IPack = {
@@ -164,6 +164,122 @@ describe("getPackFlashcards", () => {
       cardsDAL.getPackFlashcards(pack);
     }).toThrow(
       new Error("Given Pack is a Quizcard Pack, not a Flashcard Pack.")
+    );
+  });
+});
+
+describe("getPackQuizcards", () => {
+  it("gets an array of quizcards that belong to an existing pack", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = electronStore.store.packs[2];
+    expect(cardsDAL.getPackQuizcards(pack)).toEqual([
+      {
+        id: 10,
+        packId: 6,
+        type: "quiz",
+        quizType: "mc",
+        question: "what is the symbol for water?",
+        answers: [
+          { text: "H3O", correct: false },
+          { text: "H2O", correct: true },
+          { text: "B2O", correct: false },
+          { text: "W2O", correct: false },
+        ],
+        starred: false,
+      },
+      {
+        id: 11,
+        packId: 6,
+        type: "quiz",
+        quizType: "tf",
+        question: "water boils at 100 degrees celsius.",
+        answers: [
+          { text: "false", correct: false },
+          { text: "true", correct: true },
+        ],
+        starred: true,
+      },
+      {
+        id: 12,
+        packId: 6,
+        type: "quiz",
+        quizType: "chk",
+        question: "which of the following are states of matter.",
+        answers: [
+          { text: "Liquid", correct: true },
+          { text: "Gas", correct: true },
+          { text: "Gum", correct: false },
+          { text: "Sticky", correct: false },
+          { text: "Solid", correct: true },
+        ],
+        starred: false,
+      },
+      {
+        id: 13,
+        packId: 6,
+        type: "quiz",
+        quizType: "blank",
+        question: "$_$ is the study of the natural $_$.",
+        answers: [
+          { text: "Science", correct: true },
+          { text: "word", correct: true },
+        ],
+        starred: true,
+      },
+    ]);
+  });
+  it("gets an empty array for an existing pack that has no quizcards", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = electronStore.store.packs[3];
+    expect(cardsDAL.getPackQuizcards(pack)).toEqual([]);
+  });
+  it("throws an error when attempting to get quizcards for a non-existing pack", () => {
+    const electronStore: ElectronStore<ISchema> = getEmptyStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = {
+      id: 2,
+      groupId: 1,
+      name: "Does not exist",
+      type: "quiz",
+      timed: false,
+      liveResults: true,
+    };
+    expect(() => {
+      cardsDAL.getPackQuizcards(pack);
+    }).toThrow(new Error("Could not find matching Pack to get Cards from."));
+  });
+  it("throws an error when attempting to get quizcards for an existing pack with modified values", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = {
+      id: 6,
+      groupId: 2,
+      name: "Wrong name",
+      type: "quiz",
+      timed: false,
+      liveResults: false,
+    };
+    expect(() => {
+      cardsDAL.getPackQuizcards(pack);
+    }).toThrow(new Error("Could not find matching Pack to get Cards from."));
+  });
+  it("throws an error when attempting to get quizcards for a pack with a type of 'flash'", () => {
+    const electronStore: ElectronStore<ISchema> = getPopulatedStore();
+    const cardsDAL: CardsDAL = new CardsDAL(electronStore);
+    const pack: IPack = {
+      id: 6,
+      groupId: 2,
+      name: "Semester 1",
+      type: "flash",
+      timed: true,
+      liveResults: false,
+    };
+    expect(() => {
+      cardsDAL.getPackQuizcards(pack);
+    }).toThrow(
+      new Error("Given Pack is a Flashcard Pack, not a Quizcard Pack.")
     );
   });
 });
